b2829a4ff1efcb340664b47a17979656c4549181
amyskaletzky <amy.skaletzky@gmail.com>
2023-02-19 22:12:57 +0200

    submitting exerciseXP Day 2 Week 5

---
+++ Week 5/Day2/ExerciseXP/Ex1/script.js
@@ -0,0 +1,36 @@
+// Exercise 1 & 2
+
+
+// This URL is for Exercise 1
+// const URL = 'https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My'
+
+
+// This URL is for Exercise 2
+const URL = 'https://api.giphy.com/v1/gifs/search?q=sun&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My&limit=10&offset=2'
+
+
+
+
+const xhr = new XMLHttpRequest()
+xhr.open('GET', URL);
+xhr.responseType = 'json';
+xhr.send();
+
+xhr.onload = function () {
+    if (xhr.status != 200) {
+        console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+    } else {
+        console.log('object: ', xhr.response);
+    }
+};
+
+xhr.onerror = function () {
+    console.log("Request failed");
+};
+
+
+// Exercise 2 : Giphy API
+// Instructions
+// Using this part of the documention, retrieve 10 gifs about the “sun”. The starting position of the results should be 2.
+// Console.log the Javascript Object
+
