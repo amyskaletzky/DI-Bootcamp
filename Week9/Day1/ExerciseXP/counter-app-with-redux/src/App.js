3f443beb66b3b5144570c336c7a959024a308bc4
amyskaletzky <amy.skaletzky@gmail.com>
2023-03-12 13:59:03 +0200

    submitting exerciseXP

---
+++ Week9/Day1/ExerciseXP/counter-app-with-redux/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>ExerciseXP</h2>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Week9/Day1/ExerciseXP/counter-app-with-redux/src/components/Counter.js
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux';
+import { increaseCount, decreaseCount } from '../actions'
+
+const Counter = (props) => {
+    return (
+        <>
+            <button type='button' onClick={() => props.increase()}>+</button>
+            <p>Counter: {props.c}</p>
+            <button type='button' onClick={() => props.decrease()}>-</button>
+        </>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        c: state.count
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increase: () => dispatch(increaseCount()),
+        decrease: () => dispatch(decreaseCount()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
